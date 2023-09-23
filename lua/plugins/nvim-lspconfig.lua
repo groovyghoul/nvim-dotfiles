798c7871ef5b203bbdd311e69363ac6fbf15c861
Richard O'Neil <richard@bytemares.com>
2023-09-23 19:43:31 -0300

    add volar

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,31 +1,117 @@
+-- local configs = require('lspconfig.configs')
+-- local lspconfig = require('lspconfig')
+-- if not configs.golangcilsp then
+--   configs.golangcilsp = {
+--     default_config = {
+--       cmd = { 'golangci-lint-langserver' },
+--       root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+--       filetypes = { 'go' },
+--       init_options = {
+--         command = { 'golangci-lint', 'run', '--fast', '--out-format', 'json' },
+--       },
+--     },
+--   }
+-- end
+-- lspconfig.golangcilsp.setup{}
+
 return {
   {
     "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
     opts = {
-      ---@type lspconfig.options
       servers = {
+        -- Remove the old csharp_ls server setup
         csharp_ls = {},
+        volar = {},
+
+        -- Add the setup for the new vscode-csharp server
+        -- vscode_csharp = {
+        --   mason = false,
+        --   -- require("lspconfig").roslyn.setup({
+        --   --   on_attach = on_attach,
+        --   -- }),
+        -- },
       },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
       setup = {
-        -- example to setup with typescript.nvim
+        -- Remove the old csharp_ls setup function
         csharp_ls = function(_, opts)
-          -- require("csharp_ls").setup({ server = opts })
           require("lspconfig").csharp_ls.setup({
             root_dir = require("lspconfig.util").root_pattern("*.sln", ".git", "*.csproj"),
           })
           return true
         end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
+        volar = function(_, opts)
+          require("lspconfig").volar.setup({
+            filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+            init_options = {
+              preferences = {
+                disableSuggestions = true,
+              },
+              languageFeatures = {
+                implementation = true,
+                references = true,
+                definition = true,
+                typeDefinition = true,
+                callHierarchy = true,
+                hover = true,
+                rename = true,
+                renameFileRefactoring = true,
+                signatureHelp = true,
+                codeAction = true,
+                diagnostics = true,
+              },
+              -- typescript = {
+              -- tsdk = '/Users/ilias/.local/share/nvim/mason/packages/typescript-language-server/node_modules/typescript/lib',
+              -- tsdk = '/Users/ilias/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib',
+              -- tsdk = '',
+              -- },
+            },
+            on_attach = opts.on_attach,
+            capabilities = opts.capabilities,
+            -- root_dir = require("lspconfig").util.root_pattern("app.js"),
+          })
+          return true
+        end,
+
+        -- Add the setup for the vscode_csharp server
+        -- vscode_csharp = function(_, opts)
+        --   -- MAX'S VERSION
+        --   -- local lsp_configurations = require("lspconfig.configs")
+        --   -- if not lsp_configurations.roslyn then
+        --   --   lsp_configurations.roslyn = {
+        --   --     default_config = {
+        --   --       name = "roslyn",
+        --   --       cmd = {
+        --   --         "dotnet",
+        --   --         "C:\\Users\\richard\\.vscode\\extensions\\ms-dotnettools.csharp-2.0.346-win32-x64\\.roslyn\\Microsoft.CodeAnalysis.LanguageServer.exe",
+        --   --         "--logLevel=Information",
+        --   --         -- "--starredCompletionComponentPath=C:\\Users\\max.GA\\.vscode\\extensions\\ms-dotnettools.vscodeintellicode-csharp-0.1.26-win32-x64\\components\\starred-suggestions\\node_modules\\@vsintellicode\\starred-suggestions-csharp",
+        --   --         -- "--extension=C:\\Users\\max.GA\\.vscode\\extensions\\ms-dotnettools.csdevkit-0.3.21-win32-x64\\components\\roslyn-visualstudio-languageservices-devkit\\node_modules\\@microsoft\\visualstudio-languageservices-devkit\\Microsoft.VisualStudio.LanguageServices.DevKit.dll",
+        --   --         -- "--sessionId=af60e874-a2e7-41d4-8854-b6e1646b9f601686121999271",
+        --   --         -- "--telemetryLevel=all",
+        --   --         "--extensionLogDirectory=c:\\temp\\",
+        --   --       },
+        --   --       filetypes = { "cs" },
+        --   --       root_dir = require("lspconfig.util").root_pattern("*.sln", ".git", "*.csproj"),
+        --   --     },
+        --   --   }
+        --   -- end
+        --
+        --   -- RICHARD'S VERSION
+        --   require("lspconfig").vscode_csharp.setup({
+        --     cmd = {
+        --       "C:\\Users\\richard\\.vscode\\extensions\\ms-dotnettools.csharp-2.0.312-win32-x64\\.roslyn\\Microsoft.CodeAnalysis.LanguageServer.exe",
+        --       "--logLevel",
+        --       "Information",
+        --       "--extensionLogDirectory",
+        --       "c:\\temp",
+        --     },
+        --     root_dir = require("lspconfig.util").root_pattern("*.sln", ".git", "*.csproj"),
+        --   })
+        --   return true
+        -- end,
       },
       diagnostics = {
-        -- this makes the "inline diagnostics" no longer appear beside the line (also see options.lua). Using lsp-lines now, instead
         virtual_text = false,
-        -- virtual_text = true,
         virtual_lines = false,
       },
     },
